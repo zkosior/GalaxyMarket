8c28acd784098938dd3d1779731ef1f63e483456
Zbigniew Kosior <zkosior@users.noreply.github.com>
2018-02-06 01:22:21 +0000

    Added registration of conversions from basic intergalactic units to roman numerals.

---
+++ CurrencyExchangeTests/CurrencyDefinitionTests.cs
@@ -0,0 +1,65 @@
+﻿using CurrencyExchange;
+using NUnit.Framework;
+using System;
+
+namespace CurrencyExchangeTests
+{
+    [TestFixture]
+    public class CurrencyDefinitionTests
+    {
+        [Test]
+        public void WhenUnitDefinitionAdded_ConvertsToRoman()
+        {
+            var currencyDefinition = new CurrencyDefinition();
+            currencyDefinition.AddDefinition("glob", Roman.I);
+
+            Assert.AreEqual(Roman.I, currencyDefinition["glob"]);
+        }
+
+        [Test]
+        public void WhenRomanNumeralValueAlreadyRegistered_Throws()
+        {
+            var currencyDefinition = new CurrencyDefinition();
+            currencyDefinition.AddDefinition("glob1", Roman.I);
+            Assert.Throws<InvalidOperationException>(() => currencyDefinition.AddDefinition("glob2", Roman.I));
+        }
+
+        [Test]
+        public void WhenIntergalacticUnitAlreadyRegistered_Throws()
+        {
+            var currencyDefinition = new CurrencyDefinition();
+            currencyDefinition.AddDefinition("glob", Roman.I);
+            Assert.Throws<InvalidOperationException>(() => currencyDefinition.AddDefinition("glob", Roman.V));
+        }
+
+        [Test]
+        public void CoversAllSimpleRomanNumerals()
+        {
+            var currencyDefinition = new CurrencyDefinition();
+            currencyDefinition.AddDefinition("glob", Roman.I);
+            currencyDefinition.AddDefinition("prok", Roman.V);
+            currencyDefinition.AddDefinition("pish", Roman.X);
+            currencyDefinition.AddDefinition("tegj", Roman.L);
+            currencyDefinition.AddDefinition("asdf", Roman.C);
+            currencyDefinition.AddDefinition("zxcv", Roman.D);
+            currencyDefinition.AddDefinition("rfvb", Roman.M);
+
+            Assert.AreEqual(Roman.I, currencyDefinition["glob"]);
+            Assert.AreEqual(Roman.V, currencyDefinition["prok"]);
+            Assert.AreEqual(Roman.X, currencyDefinition["pish"]);
+            Assert.AreEqual(Roman.L, currencyDefinition["tegj"]);
+            Assert.AreEqual(Roman.C, currencyDefinition["asdf"]);
+            Assert.AreEqual(Roman.D, currencyDefinition["zxcv"]);
+            Assert.AreEqual(Roman.M, currencyDefinition["rfvb"]);
+        }
+
+        [Test]
+        public void WhenUnitNotRegistered_Throws()
+        {
+            var currencyDefinition = new CurrencyDefinition();
+            currencyDefinition.AddDefinition("glob", Roman.I);
+
+            Assert.Throws<InvalidOperationException>(() => { var a = currencyDefinition["prok"]; });
+        }
+    }
+}
