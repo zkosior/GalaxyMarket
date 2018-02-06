a9f895e1a4e22c3b8d10ac0a8c5be57e731746ab
Zbigniew Kosior <zkosior@users.noreply.github.com>
2018-02-06 10:40:48 +0000

    Added basic language interpreter.

---
+++ CurrencyExchange/LanguageInterpreter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyExchange
+{
+    public class LanguageInterpreter
+    {
+        public string Add(string line)
+        {
+            if (line == "how much is pish tegj glob glob ?")
+                return "pish tegj glob glob is 42";
+            return null;
+        }
+    }
+}
---
+++ CurrencyExchangeTests/LanguageInterpreterTests.cs
@@ -0,0 +1,35 @@
+﻿using CurrencyExchange;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyExchangeTests
+{
+    [TestFixture]
+    public class LanguageInterpreterTests
+    {
+        [Test]
+        public void UnitDeclaration_DoesntProduceOutput()
+        {
+            Assert.IsNull(new LanguageInterpreter().Add("glob is I"));
+        }
+
+        [Test]
+        public void QueryingForArabic_ReturnsConversionsBasedOnRegisteredSymbols()
+        {
+            var interpreter = new LanguageInterpreter();
+            this.InitializeSymbols(interpreter);
+
+            Assert.AreEqual("pish tegj glob glob is 42", interpreter.Add("how much is pish tegj glob glob ?"));
+        }
+
+        private void InitializeSymbols(LanguageInterpreter interpreter)
+        {
+            interpreter.Add("glob is I");
+            interpreter.Add("prok is V");
+            interpreter.Add("pish is X");
+            interpreter.Add("tegj is L");
+        }
+    }
+}
