88e57d02545c0aa269417ab4776786941cceb432
Zbigniew Kosior <zkosior@users.noreply.github.com>
2018-02-06 02:32:24 +0000

    Added basic convertion from roman to arabic.

---
+++ CurrencyExchange/RomanConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CurrencyExchange
+{
+    public static class RomanConverter
+    {
+        public static int ToArabic(string romanAmount)
+        {
+            if (Enum.TryParse(romanAmount, out Roman arabic))
+            {
+                return (int)arabic;
+            }
+
+            throw new ArgumentException($"Roman numeral not supported: {romanAmount}");
+        }
+    }
+}
---
+++ CurrencyExchangeTests/RomanConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using CurrencyExchange;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyExchangeTests
+{
+    [TestFixture]
+    public class RomanConverterTests
+    {
+        [Test]
+        public void ConvertsSimpleRomanToArabic()
+        {
+            Assert.AreEqual(1, RomanConverter.ToArabic("I"));
+        }
+
+        [Test]
+        public void ConvertsAllSimpleRomanToArabic()
+        {
+            Assert.AreEqual(1, RomanConverter.ToArabic("I"));
+            Assert.AreEqual(5, RomanConverter.ToArabic("V"));
+            Assert.AreEqual(10, RomanConverter.ToArabic("X"));
+            Assert.AreEqual(50, RomanConverter.ToArabic("L"));
+            Assert.AreEqual(100, RomanConverter.ToArabic("C"));
+            Assert.AreEqual(500, RomanConverter.ToArabic("D"));
+            Assert.AreEqual(1000, RomanConverter.ToArabic("M"));
+        }
+
+        [Test]
+        public void WhenCalledOnUnsupportedNumeral_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => RomanConverter.ToArabic("W"));
+        }
+    }
+}
